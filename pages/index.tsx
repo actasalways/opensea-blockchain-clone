12064a03b968869fb32cfeb420b22c3632aaa77b
Erdem <actasalways@gmail.com>
2022-04-12 23:33:41 +0300

    setting up nextjs and navbar

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Header/>
+      <Hero />
+    </>
+  )
+}
+
+export default Home
